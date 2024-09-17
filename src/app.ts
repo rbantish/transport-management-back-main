e8792641e30264b76bee5be61bb6931d4b4d7daf
rbantish <bantishramburn202100603@gmail.com>
2024-09-18 01:15:55 +0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: "*",
-    methods: ["GET","POST","PUT"]
+    methods: ["GET","POST","PUT"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]  // Headers that are allowed
 }));
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
