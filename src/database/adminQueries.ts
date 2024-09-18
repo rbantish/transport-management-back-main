ea4b4e371b645e651b13e1ab3e87e5f3393b6738
rbantish <bantishramburn202100603@gmail.com>
2024-09-18 17:10:00 +0400

    update admin queries

--- src/database/adminQueries.ts
+++ src/database/adminQueries.ts
@@ -2,7 +2,7 @@ import { Admin } from '../models/admin';
 import * as db from './queryCreator';
 
 export async function getAdminByEmail(email: string) {
-    let script = `SELECT id, name, email, password FROM Admin WHERE Email LIKE '%${email}%' LIMIT 1;`;
+    let script = `SELECT Id as id, Name as name, Email as email, Password as password FROM Admin WHERE Email LIKE '%${email}%' LIMIT 1;`;
     return await db.SelectQuery<Admin>(
         script
     );
